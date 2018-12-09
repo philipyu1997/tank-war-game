027ce59733158e13da5d2d84825651a27f08299a
pyu3 <pyu3@mail.sfsu.edu>
2018-12-08 21:35:01 -0800

    initial commit

---
+++ src/com/game/src/main/Game.java
@@ -0,0 +1,19 @@
+package com.game.src.main;
+
+import java.awt.*;
+
+/**
+ * @author Philip Yu
+ */
+public class Game extends Canvas implements Runnable {
+
+    public Game() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+} // end class Game
