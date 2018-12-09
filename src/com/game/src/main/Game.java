afdf8d8f88a4448f2b42550895ab5a15886c8f31
pyu3 <pyu3@mail.sfsu.edu>
2018-12-09 12:32:00 -0800

    added background and fixed bullet spam

--- src/com/game/src/main/Game.java
+++ src/com/game/src/main/Game.java
@@ -16,38 +16,37 @@ public class Game extends Canvas implements Runnable {
     private Thread thread;
 
     // CONSTANTS
-    private static final int SCREEN_WIDTH = 320;
-    private static final int SCREEN_HEIGHT = SCREEN_WIDTH / 12 * 9;
-    private static final int SCREEN_SCALE = 2;
+    private static final int SCREEN_WIDTH = 800;
+    private static final int SCREEN_HEIGHT = 600;
     private final String TITLE = "Game";
 
     // VARIABLES
     private BufferedImage world = new BufferedImage(SCREEN_WIDTH, SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
-    private BufferedImage player = null, bullet = null;
+    private BufferedImage player = null, bullet = null, background = null;
     private BufferedImage image = null;
+    private boolean isShooting = false;
 
     // OBJECTS
     private Player p;
     private Controller c;
 
-    private void init() {
+    public static void main(String[] args) {
 
-        requestFocus();
+        Game game = new Game();
 
-        BufferedImageLoader loader = new BufferedImageLoader();
+        game.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        game.setMaximumSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        game.setMinimumSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 
-        try {
-            player = loader.loadImage("src/assets/images/png/spritesheet/transparent/Tank1.png");
-            bullet = loader.loadImage("src/assets/images/png/spritesheet/transparent/Shell.png");
-//            image = loader.loadImage("src/assets/images/png/transparent/Tank1.png");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        JFrame frame = new JFrame(game.TITLE);
+        frame.add(game);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
-        addKeyListener(new KeyInput(this));
-
-        p = new Player(200, 200, this);
-        c = new Controller(this);
+        game.start();
 
     }
 
@@ -128,6 +127,28 @@ public class Game extends Canvas implements Runnable {
 
     }
 
+    private void init() {
+
+        requestFocus();
+
+        BufferedImageLoader loader = new BufferedImageLoader();
+
+        try {
+            player = loader.loadImage("src/assets/images/png/spritesheet/transparent/Tank1.png");
+            bullet = loader.loadImage("src/assets/images/png/spritesheet/transparent/Shell.png");
+            background = loader.loadImage("src/assets/images/bmp/Background.bmp");
+//            image = loader.loadImage("src/assets/images/png/transparent/Tank1.png");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        addKeyListener(new KeyInput(this));
+
+        p = new Player(200, 200, this);
+        c = new Controller(this);
+
+    }
+
     private void render() {
 
         BufferStrategy bs = this.getBufferStrategy();
@@ -141,6 +162,21 @@ public class Game extends Canvas implements Runnable {
         //////////////////////////////////
 
         g.drawImage(world, 0, 0, getWidth(), getHeight(), this);
+
+        int bgWidth = background.getWidth();
+        int bgHeight = background.getHeight();
+
+        int fillBgX = SCREEN_WIDTH / bgWidth;
+        int fillBgY = SCREEN_HEIGHT / bgHeight;
+
+        /**
+         * Apply background image to map
+         */
+        for (int col = 0; col <= fillBgY; ++col) {
+            for (int row = 0; row <= fillBgX; ++row) {
+                g.drawImage(background, row * bgWidth, col * bgHeight, bgWidth, bgHeight, null);
+            }
+        }
 
         p.render(g);
         c.render(g);
@@ -164,7 +200,8 @@ public class Game extends Canvas implements Runnable {
             p.setVelY(5);
         } else if (key == KeyEvent.VK_UP) {
             p.setVelY(-5);
-        } else if (key == KeyEvent.VK_SPACE) {
+        } else if (key == KeyEvent.VK_SPACE && !isShooting) {
+            isShooting = true;
             c.addBullet(new Bullet(p.getX(), p.getY(), this));
         } else if (key == KeyEvent.VK_Q) {
             System.out.println("\nExiting...");
@@ -185,27 +222,9 @@ public class Game extends Canvas implements Runnable {
             p.setVelY(0);
         } else if (key == KeyEvent.VK_UP) {
             p.setVelY(0);
+        } else if (key == KeyEvent.VK_SPACE) {
+            isShooting = false;
         }
-
-    }
-
-    public static void main(String[] args) {
-
-        Game game = new Game();
-
-        game.setPreferredSize(new Dimension(SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE));
-        game.setMaximumSize(new Dimension(SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE));
-        game.setMinimumSize(new Dimension(SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE));
-
-        JFrame frame = new JFrame(game.TITLE);
-        frame.add(game);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-
-        game.start();
 
     }
 
